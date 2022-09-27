b9edd960d3dce6d9f5f5bec1355ce00583a379fa
Dallin Scott <dallinscott743@gmail.com>
2022-09-27 10:27:28 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Tic Tac Toe
+// Dallin Scott
+
+namespace review_functions // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($@"
+This is the game Tic-Tac-Toe!
+        created by: Dallin Scott
+        ");
+
+            Console.WriteLine();
+            
+        }
+    }
+}
